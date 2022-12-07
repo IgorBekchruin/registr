8df341b158dfe8189e9390ebbe24ced979946838
IgorBekchruin <worki.iba@gmail.com>
2022-12-07 22:31:13 +0400

    firs

---
+++ users/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.views.generic import View
+from .forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+
+
+class Register(View):
+    template_name = 'registration/register.html'
+
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+        return render(request, self.template_name, {'form': form})
